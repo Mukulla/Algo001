3d958d50fbca52052df9d033b812bbb15852d3d3
Mukulla <mbozon@mail.ru>
2021-04-19 09:43:21 +0300

    List Added

---
+++ Algo001/Algo002/MaLinkedList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algo002
+{
+    class MaLinkedList : ILinkedList
+    {
+        //Общее количество элементов
+        int Count;
+
+        //Текущий элемент
+        Node CurrentElement;
+
+        //Первый элемент списка
+        Node Start;
+        //Последний элемент списка
+        Node End;
+
+        public void AddNode(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveNode(Node node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
