ebbe424e962b6fe4d18eada99675d15de2b40c2e
Mukulla <mbozon@mail.ru>
2021-04-19 09:27:31 +0300

    New Proj Created

---
+++ Algo001/Algo002/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Algo002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
